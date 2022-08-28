d8c13b9a6b27392f7a389323ded5de37f0206d11
DESKTOP-O0DNJK9\ZX <359874259@qq.com>
2022-08-28 20:52:22 +0800

    init project

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="box">
+    <div class="box-left"></div>
+    <div class="box-middle"></div>
+    <div class="box-right"></div>
+  </div>
+</template>
+
+<script setup lang="ts">
+</script>
+
+<style lang='scss' scoped>
+.box {
+  display: flex;
+  height: 100%;
+  &-left {
+    width: 400px;
+    background: red;
+  }
+  &-middle {
+    flex: 1;
+  }
+  &-right {
+    width: 400px;
+    background: green;
+  }
+}
+</style>

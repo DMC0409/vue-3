908ebabc1545514502da82ad474b7fa1936b5dd7
DESKTOP-O0DNJK9\ZX <359874259@qq.com>
2022-08-31 21:58:02 +0800

    css finish

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="box">
+  <div class="box" :style="{ background: `url(${bg}) no-repeat`,backgroundSize:'100% 100%'}">
     <div class="box-left"></div>
     <div class="box-middle"></div>
     <div class="box-right"></div>
@@ -7,6 +7,7 @@
 </template>
 
 <script setup lang="ts">
+import bg from "@/assets/1.jpg";
 </script>
 
 <style lang='scss' scoped>
@@ -15,14 +16,12 @@
   height: 100%;
   &-left {
     width: 400px;
-    background: red;
   }
   &-middle {
     flex: 1;
   }
   &-right {
     width: 400px;
-    background: green;
   }
 }
 </style>
--- src/stores/counter.ts
+++ src/stores/counter.ts
@@ -3,14 +3,10 @@ import { defineStore } from 'pinia'
 export const useCounterStore = defineStore({
   id: 'counter',
   state: () => ({
-    counter: 0
+    list: {}
   }),
   getters: {
-    doubleCount: (state) => state.counter * 2
   },
   actions: {
-    increment() {
-      this.counter++
-    }
   }
 })
